cf5e08d760c3eea4ccb75bc0fcc4b5dc3ae55129
yashiro81919 <yashiro81919@hotmail.com>
2024-05-12 15:38:36 +0800

    adjust

--- pes6/main.py
+++ pes6/main.py
@@ -156,7 +156,7 @@ def wait_end():
             pyautogui.click(x, y)
             next_step()            
         except pyautogui.ImageNotFoundException:
-            pyautogui.sleep(10)
+            pyautogui.sleep(12)
             pydirectinput.press('b')
             print('not in end game menu')
             wait_end()         
